9a348e780ae9698d18bb8f45ce080fb4c86d2dd6
reddowell <vikt.anmail@gmail.com>
2018-12-05 23:15:32 +0300

    main page basis

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="vkzhuk">
+
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+
+<!-- Begin page content -->
+<main role="main" class="container">
+    <h1 class="mt-5">Title</h1>
+    <p class="lead">Text</p>
+</main>
+
+<footer class="footer">
+    <div class="container">
+        Footer
+    </div>
+</footer>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', ['as' => 'index', function () use ($router) {
-    return $router->app->version();
-}]);
+$router->get('/', ['as' => 'index', 'uses' => 'HomeController@index']);
 
 $router->get('/{width:[0-9]+}/{height:[0-9]+}[/{name}]', ['as' => 'image.place',
     'uses' => 'ImageController@placeImage']);
